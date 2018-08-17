4c2580f8578808b70370dc99ce8b7c7e5e44332f
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-08-17 15:34:30 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "=";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
